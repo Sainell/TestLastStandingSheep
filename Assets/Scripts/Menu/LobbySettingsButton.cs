dc3a6e18450293dee5ba34253ee48873998bf8f3
Sainell <>
2020-09-25 20:16:21 +0300

    add menu

---
+++ Assets/Scripts/Menu/LobbySettingsButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+namespace LastStandingSheep
+{
+    public sealed class LobbySettingsButton : MonoBehaviour, IPointerClickHandler
+    {
+        private Canvas _menuPrefab;
+        private Canvas _interface;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            _menuPrefab.enabled = true;
+            _interface.enabled = false;
+
+        }
+
+        private void Awake()
+        {
+            _menuPrefab = GameObject.Find("Settings").GetComponent<Canvas>();
+            _interface = gameObject.transform.parent.parent.GetComponent<Canvas>();
+        }
+    }
+}
